35716cf8a0c8ffa17c9427fa4e02f607617b37d4
AlexeiFedya <fedya5711@icloud.com>
2020-11-24 01:49:42 +0300

    heroku

--- app.js
+++ app.js
@@ -5,14 +5,15 @@ const path = require('path')
 
 const app = express()
 
+
 app.use(express.json({ extended: true}))
 
 app.use('/api/mess', require('./routes/mess.routes'))
 
 if(process.env.NODE_ENV === "production") {
-    app.use('/', express.static(path.join(__dirname, 'client', 'build')))
+    app.use(express.static(path.join(__dirname, 'client', 'build')))
 
-    app.get('*', (req, res) => {
+    app.get((req, res) => {
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 }
@@ -21,7 +22,7 @@ const PORT = config.get('port') || 5000
 
 async function start () {
     try {
-        await mongoose.connect(config.get('mongoUri'), {
+        await mongoose.connect(process.env.MONGODB_URI || config.get('mongoUri'), {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true
